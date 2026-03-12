aed3b59466cf2ca5b28ffa3fdb675a4205f69571
rafadinero22 <rafaeljesus694@outlook.com>
2026-03-11 19:07:46 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+// password stored in environment variable
+const password = process.env.APP_PASSWORD;
+
+app.get("/login", function(req, res){
+
+    const username = req.query.username;
+    const userpassword = req.query.password;
+
+    if(userpassword === password){
+        console.log("User logged in:", username);
+        res.send("Welcome " + username);
+    } else {
+        res.send("Access denied");
+    }
+
+});
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
